4e1888b120de2435d8dae24c2452ae80533babd3
kamoonty <kamoonty@gmail.com>
2019-07-22 20:44:58 +0700

    edit vl name space from odom to vl/odom

---
+++ build/mecanum_sim/CMakeFiles/mecanum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mecanum.dir/src/mecanum_controller.cc.o"
+  "/home/kanin/catkin_ws_roboware/devel/lib/libmecanum.pdb"
+  "/home/kanin/catkin_ws_roboware/devel/lib/libmecanum.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mecanum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/mecanum_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kanin/catkin_ws_roboware/src/mecanum_sim
+# Build directory: /home/kanin/catkin_ws_roboware/build/mecanum_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
